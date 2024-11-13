359745164e11840fa91f57a6b428d58004f0b88a
Arthur Vaz <arthur.vaz95@outlook.com>
2024-11-12 23:01:26 -0300

    add controller

---
+++ ink-ribbon-profile-api/ink-ribbon-profile-api/Controller/AchievementController.cs
@@ -0,0 +1,57 @@
+﻿using ink_ribbon_profile_api.Domain.Interfaces.Services;
+using Microsoft.AspNetCore.Cors;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ink_ribbon_profile_api.Controller
+{
+    [ApiController]
+    [EnableCors("All")]
+    public class AchievementController : ControllerBase
+    {
+        private readonly ILogger<AchievementController> _logger;
+        private readonly ISteamUserSevice _steamUserSevice;
+        private readonly IXboxAchievementService _xboxAchievementService;
+
+        public AchievementController(ILogger<AchievementController> logger, ISteamUserSevice steamUserSevice, IXboxAchievementService xboxAchievementService)
+        {
+            _logger = logger;
+            _steamUserSevice = steamUserSevice;
+            _xboxAchievementService = xboxAchievementService;
+        }
+
+        [HttpGet("steam/GetSteamIdByName")]
+        public async Task<IActionResult> GetSteamIdByName(string steamName)
+        {
+            //var command = new GetConditionsQuery(assetId);
+            var user = await _steamUserSevice.BuildSteamUser(steamName);
+            return Ok(user);
+        }
+
+        [HttpGet("steam/GetUser")]
+        public async Task<IActionResult> GetSteamUserById(string steamId)
+        {
+            //var command = new GetConditionsQuery(assetId);
+            var user = await _steamUserSevice.GetSteamUserById(steamId);
+
+            return Ok(user);
+        }
+
+        [HttpGet("Xbox/GetAchievements")]
+        public async Task<IActionResult> GetAchievements()
+        {
+            //var command = new GetConditionsQuery(assetId);
+            var user = await _xboxAchievementService.GetAchievements();
+
+            return Ok(user);
+        }
+
+        [HttpGet("Xbox/GetAchievementsById")]
+        public async Task<IActionResult> GetAchievementsById(string xuid)
+        {
+            //var command = new GetConditionsQuery(assetId);
+            var user = await _xboxAchievementService.GetAchievementsById(xuid);
+
+            return Ok(user);
+        }
+    }
+}
